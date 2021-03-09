a8aa9c2ba4613b824dc2569552ea1e0cb9d6a28c
Renua <ohirorenua@gmail.com>
2021-03-09 11:50:58 +0100

    10commits

---
+++ lessons.js
@@ -0,0 +1,83 @@
+let lessondata=[
+    {
+        id: 1001,
+        subject: "Math",
+        location: "Akure",
+        price: 100,
+        space: 5,
+        image: "subjectimage/math_fullsize.jpg"
+    },
+    {
+        id: 1002,
+        subject: "Physics",
+        location: "Lagos",
+        price: 200,
+        space: 5,
+        image: "subjectimage/physics_fullsize.jpg"
+    },
+    {
+        id: 1003,
+        subject: "Chemistry",
+        location: "Gombe",
+        price: 300,
+        space: 5,
+        image: "subjectimage/chemistry_fullsize.jpg"
+    },
+    {
+        id: 1004,
+        subject: "Biology",
+        location: "Enugu",
+        price: 400,
+        space: 5,
+        image: "subjectimage/biology_fullsize.jpg"
+    },
+    {
+        id: 1005,
+        subject: "Engish",
+        location: "Minna",
+        price: 500,
+        space: 5,
+        image: "subjectimage/english_fullsize.jpg"
+    },
+    {
+        id: 1006,
+        subject: "Literature",
+        location: "Zaria",
+        price: 600,
+        space: 5,
+        image: "subjectimage/literature_fullsize.jpg"
+    },
+    {
+        id: 1007,
+        subject: "Music",
+        location: "Abuja",
+        price: 700,
+        space: 5,
+        image: "subjectimage/music_fullsize.jpg"
+    },
+    {
+        id: 1008,
+        subject: "Art",
+        location: "Benin",
+        price: 800,
+        space: 5,
+        image: "subjectimage/art_fullsize.jpg"
+    },
+    {
+        id: 1009,
+        subject: "Sports",
+        location: "Jos",
+        price: 900,
+        space: 5,
+        image: "subjectimage/sports_fullsize.jpg"
+    },
+    {
+        id: 1010,
+        subject: "Dance",
+        location: "Jos",
+        price: 1000,
+        space: 5,
+        image: "subjectimage/dance_fullsize.jpg"
+    }
+]
+//export default lessondata 
